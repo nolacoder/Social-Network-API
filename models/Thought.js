99a3c7e1fd411eb9b677aa60d866ab8cd2dd1ab8
Jared <jjohns3@tulane.edu>
2022-10-29 23:22:02 -0500

    Thought.js model mostly complete

---
+++ models/Thought.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: [1, "Please enter at least one character"],
+            max: [200, "You have exceeded the limit of 200 characters"]
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+
+        username: {
+            type: String,
+            required: true
+        },
+
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+    // Create getter function
+)
