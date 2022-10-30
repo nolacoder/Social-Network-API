9597f9595e3625353c557bc983bb2cb3f17f1180
Jared <jjohns3@tulane.edu>
2022-10-30 00:08:45 -0500

    user and thought controller buildouts

---
+++ controllers/thoughtController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    getThoughts(req, res) {
+        
+    },
+    createThought(req, res) {
+        
+    },
+    getSingleThought(req, res) {
+        
+    },
+    updateThought(req, res) {
+        
+    },
+    deleteThought(req, res) {
+        
+    },
+    createReaction(req, res) {
+        
+    },
+    deleteReaction(req, res) {
+        
+    }
+}
---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    getUsers(req, res) {
+
+    },
+    createUser(req, res) {
+        
+    },
+    getSingleUser(req, res) {
+        
+    },
+    updateUser(req, res) {
+        
+    },
+    deleteUser(req, res) {
+        
+    },
+    addFriend(req, res) {
+        
+    },
+    removeFriend(req, res) {
+        
+    }
+}
